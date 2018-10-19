5902b1e7dd2d28c2bed45cd01038edf25eddc74c
priteshsingh-vats <42402753+priteshsingh-vats@users.noreply.github.com>
2018-10-20 01:33:06 +0530

    Update HelloWorld.c
    i have added some header files to make code better to understand

--- Hello_World/HelloWorld.c
+++ Hello_World/HelloWorld.c
@@ -3,8 +3,11 @@ To print "Hello World!"
 Written by: Joshua D'Cunha
 Written on: 25/10/2016
 */
-
-main()
+#include<stdio.h>
+ int main()
 {
+     printf("east or west hello world is best");
     printf("Hello World!\n");
+     printf("we finally run the program");
+     return 0;
 }
